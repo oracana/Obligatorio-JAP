2b1ea07b272a9ceb9e011a2ab76f578361cff65f
oracana <oracana49@gmail.com>
2020-08-27 12:05:41 -0300

    logout

--- js/init.js
+++ js/init.js
@@ -39,23 +39,6 @@ var getJSONData = function(url){
         return result;
     });
 }
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-
-  localStorage.setItem("usuario", profile.getName());
-  location.href="home.html";
-}
 
 function signOut(){
   var auth2 = gapi.auth2.getAuthInstance();
@@ -67,11 +50,11 @@ function signOut(){
 location.href="index.html";
 }
 
-function onLoad(){
+/*function onLoad(){
   gapi.load ('auth2', function(){
     gapi.auth2.init();
   });
-}
+}*/
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
