cb9ca522d547acaaa4b64fdc2a2f0c4e3a356081
oracana <oracana49@gmail.com>
2020-08-28 13:03:20 -0300

    prueba logout

--- js/login-google.js
+++ js/login-google.js
@@ -16,24 +16,5 @@ function onSignIn(googleUser) {
     location.href="home.html";
   }
 
+
   
-  /*function onLoad(){
-    gapi.load ('auth2', function(){
-      gapi.auth2.init();
-    });
-  }*/
-
-  function signOut(){
-    gapi.load ('auth2', function(){
-      gapi.auth2.init();
-    });
-
-    var auth2 = gapi.auth2.getAuthInstance();
-    
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-
-    localStorage.clear();
-    location.href="index.html";
-  }
