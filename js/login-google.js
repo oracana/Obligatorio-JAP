35b598f7a2188f94c92e1d897b9c3404e7060d7d
oracana <oracana49@gmail.com>
2020-08-28 12:39:01 -0300

    signout

--- js/login-google.js
+++ js/login-google.js
@@ -16,7 +16,7 @@ function onSignIn(googleUser) {
     location.href="home.html";
   }
 
-  /*
+  
   function signOut(){
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
@@ -25,10 +25,10 @@ function onSignIn(googleUser) {
 
     localStorage.clear();
     location.href="index.html";
-  }*/
+  }
 
-  /*function onLoad(){
+  function onLoad(){
   gapi.load ('auth2', function(){
     gapi.auth2.init();
   });
-}*/
+}
