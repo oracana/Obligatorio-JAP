dc394bd1b352c3ad5c2a3aef000ab089fa70fdae
oracana <oracana49@gmail.com>
2020-10-01 22:51:11 -0300

    el carrito casi que funcional

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,63 @@
+var shipping = 0;
+var articles = [];
+
 //función para mostrar los elementos del carrito
 function showCart(array){
     let html=``;
     for(i=0; i<array.length; i++){
         article = array[i];
-        subtotal = article.unitCost*article.count;
         html+= `
         <tr>
-      <th class="align-middle" scope="row">${article.count}</th>
-      <td class="align-middle"><img style='height:4em' src='${article.src}' alt='${article.src}' class="img-thumbnail"></td>
-      <td class="align-middle">${article.name}</td>
-      <td class="align-middle">${article.unitCost} ${article.currency}</td>
-      <td class="align-middle">${subtotal} ${article.currency}</td>
-      </tr>
-        `;
-    }
+        <th class="align-middle align-center" scope="row"><img style='height:4em' src='${article.src}' alt='${article.src}' class="img-thumbnail"></th>
+        <td class="align-middle">${article.name}</td>
+        <td class="align-middle">
+        <div>
+                      <div class="def-number-input number-input btn-group">
+                        <button onclick="this.parentNode.querySelector('input[type=number]').stepDown(); showTotal(articles);"
+                          class="btn btn-outline-secondary"><strong>-</strong>
+                          </button>
+                        <input class="quantity" min="1" name="contador[${i}]" value="${article.count}" type="number" id="contador${i}" style="width:3em; text-align:center">
+                        <button onclick="this.parentNode.querySelector('input[type=number]').stepUp(); showTotal(articles);"
+                          class="btn btn-outline-secondary"><strong>+</strong></button>
+                      </div>
+                    </div>   
+  </td>
+  <td class="align-middle">${article.unitCost} ${article.currency}</td>
+  </tr>
+  `;
+}
+           
     document.getElementById("cartList").innerHTML= html;
-    document.getElementById("cartCount").innerHTML= array.length;
 }
 
 function showTotal(array){
-    var total = 0;
-    for(let article of array){
-        let subtotal = article.unitCost*article.count;
+       var subtotal = 0;
+       var genCount = 0;
+
+    for(i=0; i<array.length; i++){
+        article = array[i];
+        let counterID = "contador"+i;
+        let counter = document.getElementById(counterID).value;
+        
+        genCount += parseInt(counter);
+
         if(article.currency == "UYU") {
-            total += subtotal/40;  
+            subtotal += article.unitCost*counter/40; 
         } else if(article.currency == "USD") {
-            total += subtotal;
+            subtotal += article.unitCost*counter;
         }
+        
     }
+    var total = subtotal;
+
+    if(shipping!=0){ 
+        total = subtotal + subtotal*shipping;
+    }
+
+    document.getElementById("cartCount").innerHTML= genCount;
+    document.getElementById("envio").innerHTML= (subtotal*shipping).toFixed(2) + ` USD`
     document.getElementById("totalCost").innerHTML= total + ` USD`;
+    document.getElementById("subtot").innerHTML= subtotal + ` USD`;
 }
 
 
@@ -41,9 +70,26 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            var articles = resultObj.data.articles
+            articles = resultObj.data.articles;
             showCart(articles);
             showTotal(articles);
         }
     });
+
+    //cambios en el valor del shipping
+    document.getElementById("tipoEnvio1").addEventListener("change", function(){
+        shipping = 0.15;
+        showTotal(articles);
+    });
+    
+    document.getElementById("tipoEnvio2").addEventListener("change", function(){
+        shipping = 0.07;
+        showTotal(articles);
+    });
+
+    document.getElementById("tipoEnvio3").addEventListener("change", function(){
+        shipping = 0.05;
+        showTotal(articles);
+    });
 });
+
