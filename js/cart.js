c52046a11cb80de76a5ac0ddefc631dd009f59bf
Oracana <gorriarancarolina@gmail.com>
2020-10-18 14:24:31 -0300

    cart

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,13 @@
 var shipping = 0;
 var articles = [];
 var success = "";
-var cartQuantity = 0;
+
+//genero un array con todos los span que se ingresan bajo los input (main), para la validacion
+var mainValidationSpans = document.querySelectorAll(".validacion");
+
+//idem para los span de los input en el modal
+var creditCardValidationSpans = document.querySelectorAll(".validacionTarjeta");
+
 
 //función para mostrar los elementos del carrito
 function showCart(array) {
@@ -88,45 +94,127 @@ function showTotal(array) {
     document.getElementById("envio").innerHTML = (subtotal * shipping).toFixed(2) + " " + currencySelected;
     document.getElementById("totalCost").innerHTML = (total).toFixed(2) + " " + currencySelected;
     document.getElementById("subtot").innerHTML = (subtotal).toFixed(2) + " " + currencySelected;
-
-
 }
 
-//función para chequear que la información de compra no esté vacía, para validarla. Aún no está en uso
-function infoCheck() {
-    let addrStreet = document.getElementById("dirCalle").value;
-    let addrNum = document.getElementById("dirNum").value;
-    let addrInt = document.getElementById("dirEsq").value;
+//función para validar la compra.
+function mainCheck() {
+    let creditCardSelected = document.getElementById("formaDePago1").checked;
+    let bankTransfSelected = document.getElementById("formaDePago2").checked;
 
-    let cardName = document.getElementById("titularTarjeta").value;
-    let cardNumber = document.getElementById("numeroTarjeta").value;
-    let cardExpiration = document.getElementById("vencTarjeta").value;
-    let cardCVV = document.getElementById("cvvTarjeta").value;
+    //chequeo de todos los spans con la clase "validacion", vinculados a los input de dirección de envío.
+    //también se incluye la validación asociada a la selección de un método de pago
+    for (let i = 0; i < mainValidationSpans.length; i++) {
+        let element = mainValidationSpans[i];
 
-    let transfNumber = document.getElementById("transferCuenta").value; 
+        let inputID = document.getElementById(element.id + "-input");
+        let inputIDValue = document.getElementById(element.id + "-input").value;
+        let spanHTML = document.getElementById(element.id);
 
-    if(addrStreet != "" && addrNum != "" && addrInt != "" && (transfNumber != "" || cardName != "" && cardNumber != "" && cardExpiration != "" && cardCVV != "")) {
-        alert(success);
-    } else if (addrStreet == "" && addrNum == "" && addrInt == "") {
-        alert("Por favor complete los datos de envío");
-    } else if(transfNumber == "" || cardName == "" && cardNumber == "" && cardExpiration == "" && cardCVV == ""){
-        alert("Por favor complete la información de pago");
-    } 
+        if (inputIDValue == "" || inputIDValue == "País" || (creditCardSelected == false && bankTransfSelected == false)) {
+            inputID.classList.remove("is-valid");
+            inputID.classList.add("is-invalid");
+            spanHTML.classList.add("invalido");
+            spanHTML.innerHTML = `Debe ingresar ${element.id}`;
+        } else {
+            inputID.classList.remove("is-invalid");
+            inputID.classList.add("is-valid");
+            spanHTML.classList.remove("invalido");
+            spanHTML.classList.add("valido");
+        }
+    }
+
+    //chequeo de selección de un método de envío
+    let shipping1Selected = document.querySelector("#tipoEnvio1").checked;
+    let shipping2Selected = document.querySelector("#tipoEnvio2").checked;
+    let shipping3Selected = document.querySelector("#tipoEnvio3").checked;
+    if(shipping1Selected == false & shipping2Selected == false & shipping3Selected == false){
+        document.getElementById("validacionEnvios").classList.remove("valido");
+        document.getElementById("validacionEnvios").classList.add("invalido");
+        document.getElementById("validacionEnvios").innerHTML = "Debe seleccionar un método de envío";
+    } else{
+        document.getElementById("validacionEnvios").classList.remove("invalido");
+        document.getElementById("validacionEnvios").classList.add("valido");
+    }
+    
+    //chequeo final: que los 7 span con clase "validacion" estén validados y que se haya seleccionado un método de envío.
+    let mainValidatedSpans = document.querySelectorAll(".validacion.valido");
+    if(mainValidatedSpans.length = 7  & (shipping1Selected || shipping2Selected || shipping3Selected)){
+        //Sweet Alert
+        Swal.fire({
+            icon: 'success',
+            title: 'Felicidades',
+            text: success,
+            showConfirmButton: false,
+            footer: '<a href="home.html">Volver al inicio</a>',
+          })
+    }
 }
 
-// function cardCheck(){
-//     let cardName = document.getElementById("titularTarjeta").value;
-//     let cardNumber = document.getElementById("numeroTarjeta").value;
-//     let cardExpiration = document.getElementById("vencTarjeta").value;
-//     let cardCVV = document.getElementById("cvvTarjeta").value;
+//validación de la información del modal de pago
+function cardCheck() {
+    let creditCardSelected = document.getElementById("formaDePago1").checked;
+    let bankTransfSelected = document.getElementById("formaDePago2").checked;
 
-//     let transfNumber = document.getElementById("transferCuenta").value; 
+    //se separa en tres posibilidades: 
+    //1. haber seleccionado la tarjeta de crédito -- si todos los campos se completaron, al guardar los datos se cierra el modal.
+    //2. haber seleccionado la transferencia bancaria -- si se completa el número de cuenta, al guardar los datos se cierra el modal.
+    //3. no haber seleccionado ningún método, ante lo cual se muestra un cartel requiriendo seleccionar uno.
+    if (creditCardSelected) {
+        document.getElementById("modal-de-pago").classList.remove("invalido");
+        document.getElementById("modal-de-pago").classList.add("valido");
+        
+        for (let i = 0; i < creditCardValidationSpans.length; i++) {
+            let element = creditCardValidationSpans[i];
+            
+            let inputID = document.getElementById(element.id + "-input");
+            let inputIDValue = document.getElementById(element.id + "-input").value;
+            let spanHTML = document.getElementById(element.id);
+            
+            if (inputIDValue == "") {
+                inputID.classList.remove("is-valid");
+                inputID.classList.add("is-invalid");
+                spanHTML.classList.add("invalido");
+                spanHTML.innerHTML = `Debe ingresar ${element.id}`;
+            } else {
+                inputID.classList.remove("is-invalid");
+                inputID.classList.add("is-valid");
+                spanHTML.classList.remove("invalido");
+                spanHTML.classList.add("valido");
+            }
+        }
+        
+        let creditCardValidatedSpans = document.querySelectorAll(".validacionTarjeta.valido");
+        if(creditCardValidatedSpans.length == 4){
+            //método de bootstrap
+            $('#modalPago').modal('hide');
+        }
+        
+    } else if (bankTransfSelected) {
+        let transfNumber = document.getElementById("cuenta-input");
+        let transfNumberSpan = document.getElementById("cuenta");
 
-//     if(transfNumber == "" || cardName == "" && cardNumber == "" && cardExpiration == "" && cardCVV == ""){
-//         alert("Por favor complete la información de pago");
-//     } 
-// }
+        if(transfNumber.value == ""){
+            transfNumber.classList.remove("is-valid");
+            transfNumber.classList.add("is-invalid");
+            transfNumberSpan.classList.add("invalido");
+            transfNumberSpan.innerHTML = `Debe ingresar número de cuenta`;
+        } else {
+            transfNumber.classList.remove("is-invalid");
+            transfNumber.classList.add("is-valid");
+            transfNumberSpan.classList.remove("invalido");
+            transfNumberSpan.classList.add("valido");
+            //método de bootstrap
+            $('#modalPago').modal('hide');
+        }
+        
+    } else {
+        document.getElementById("modal-de-pago").classList.remove("valido");
+        document.getElementById("modal-de-pago").classList.add("invalido");
+        document.getElementById("modal-de-pago").innerHTML = "Por favor seleccione un método de pago";
+    }
 
+    
+}
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -150,7 +238,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 <option>${country.name}</option>
                 `
             }
-            document.getElementById("paises").innerHTML = countrySelect;
+            document.getElementById("pais-input").innerHTML = countrySelect;
         }
     });
 
@@ -181,14 +269,14 @@ document.addEventListener("DOMContentLoaded", function (e) {
     document.getElementById("moneda").addEventListener("change", function () {
         showTotal(articles);
     });
-    
+
     //seleccion de metodo de pago
     document.getElementById("formaDePago1").addEventListener("click", function () {
-        document.getElementById("formaPago").innerHTML = "Tarjeta de crédito";
+        document.getElementById("metodo-input").innerHTML = "Tarjeta de crédito";
     });
     document.getElementById("formaDePago2").addEventListener("click", function () {
-        document.getElementById("formaPago").innerHTML = "Transferencia bancaria";
+        document.getElementById("metodo-input").innerHTML = "Transferencia bancaria";
     });
-    
+
 });
 
