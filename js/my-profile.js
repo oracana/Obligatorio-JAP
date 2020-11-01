95d13086d6e712ba121d5abf56923dbe3df17622
Oracana <gorriarancarolina@gmail.com>
2020-11-01 02:32:37 -0300

    comments so we dont forget

--- js/my-profile.js
+++ js/my-profile.js
@@ -39,32 +39,37 @@ cancelBtn.addEventListener("click", function(){
     cancel();
     location.href = "/my-profile.html";
 });
-const request = new XMLHttpRequest();
 //intentando subir la imagen al servidor de imgBB
-const IMGBB_API_URL = 'https://api.imgbb.com/1/upload?key=5a578c1ecd8a2f86a6ecc589c8b47317';
 inputImg.addEventListener('change', function(e){
+    //esta es la url a la que enviaremos la imagen
+    const IMGBB_API_URL = 'https://api.imgbb.com/1/upload?key=5a578c1ecd8a2f86a6ecc589c8b47317';
+    //y una variable con el contenido de la imagen subida al input type file.
     const imgUploaded = e.target.files[0];
+    //tambien generamos una nueva XMLHttpRequest. esto es porque vamos a estar llamando a esta request en repetidas ocasiones.
+    const request = new XMLHttpRequest();
+    
+    //variable de tipo formData para enviar la request con la imagen para subir a nuestro servidor externo.
     var form = new FormData();
     form.append("image", imgUploaded)
-    console.log(form);
     
 
-    //hacemos una XMLHttpRequest de tipo post al servidor de imgbb
+    //estas dos líneas definen nuestro pedido de subida de información. primero aclaramos el método y la url destino
+    //después el contenido. Esto es en el mismo orden en el que se aclara en la página de imgbb.
     request.open("POST", IMGBB_API_URL);
     request.send(form);
     
-    console.log(request);
-    
+    //lo que sigue nos permite trabajar con la respuesta del servidor.    
     request.onreadystatechange = function () {
         // In local files, status is 0 upon success in Mozilla Firefox
         if(request.readyState === XMLHttpRequest.DONE) {
           var status = request.status;
           if (status === 0 || (status >= 200 && status < 400)) {
-            // The request has been completed successfully
+            // luego de obtener una respuesta exitosa, parseamos la información de la respuesta y tomamos la url de la imagen ya pública.
             resultObj = request.response;
             resultObj = JSON.parse(resultObj);
             
             imgNewUrl = resultObj.data.url;
+            //ya que estamos, en la misma función podemos mostrar la imagen.
             showImg.src = imgNewUrl;
           } else {
             console.log('Oh no! There has been an error with the request!');
